0ad596bcc25cd723082b0b0d93183e20adc48ac4
farhan-navas <farhanmnavas@gmail.com>
2026-03-13 18:09:08 -0700

    export map error

--- frontend/lib/source.ts
+++ frontend/lib/source.ts
@@ -1,6 +1,6 @@
 import { docs } from "collections/server";
 import { loader } from "fumadocs-core/source";
-import { lucideIconsPlugin } from "fumadocs-core/source/plugins/lucide-icons";
+import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
 
 export const source = loader({
   baseUrl: "/docs",
